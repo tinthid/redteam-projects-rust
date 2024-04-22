50d02af3385618ec5725a29ff50d4de59c2fa84b
Pimukthee Jaikla <pimukthee@gmail.com>
2024-04-22 20:14:09 +0700

    refactor tcp port scanner

--- basic-tcp-port-scanner/src/main.rs
+++ basic-tcp-port-scanner/src/main.rs
@@ -1,50 +1,45 @@
-use std::io::prelude::*;
-use std::net::TcpStream;
-use std::env;
-use tokio::runtime::Builder;
 use futures::future::join_all;
+use std::{
+    env,
+    net::{Ipv4Addr, SocketAddrV4},
+};
+use tokio::{io::AsyncWriteExt, net::TcpStream, runtime::Builder, task};
 
-fn scan_port(ip_addr: String, port: i32) -> std::io::Result<()> {
-    let mut stream = TcpStream::connect(format!("{ip_addr}:{port}"))?;
-    stream.write(&[1])?;
-    stream.read(&mut [0; 128])?;
-    Ok(())
+type Result<T> = std::result::Result<T, Error>;
+type Error = Box<dyn std::error::Error + Send + Sync>;
+
+async fn scan_port(socket: SocketAddrV4) -> Result<u16> {
+    let mut stream = TcpStream::connect(socket).await?;
+    stream.shutdown().await?;
+
+    Ok(socket.port())
 }
 
-fn main() {
-
+fn main() -> Result<()> {
     let ip_addr = env::args().nth(1).expect("no ip address given");
+    let ipv4 = ip_addr.parse::<Ipv4Addr>()?;
 
     let runtime = Builder::new_multi_thread()
-    .worker_threads(256) 
-    .enable_all()     
-    .build()
-    .unwrap();
-
+        .worker_threads(256)
+        .enable_all()
+        .build()
+        .unwrap();
 
     runtime.block_on(async {
         let mut tasks = vec![];
-        for p in 1..1024 {
-            let ip_addr_clone = ip_addr.clone();
-            tasks.push(tokio::spawn(async move {
-                match scan_port(ip_addr_clone, p) {
-                    Ok(_) => Some(p),
-                    _ => None,
-                }
-            }));
+        for p in 1..2010 {
+            let socket = SocketAddrV4::new(ipv4, p);
+            tasks.push(task::spawn(scan_port(socket)));
         }
-
 
         let results = join_all(tasks).await;
-        let open_ports: Vec<i32> = results.into_iter()
-        .filter_map(|r| r.unwrap()) 
-        .collect();
-
-        for port in open_ports {
-            println!("Port {} is open", port);
+        for r in results {
+            match r.unwrap() {
+                Err(e) => println!("{e:?}"),
+                Ok(p) => println!("Open port {p}"),
+            }
         }
-
     });
 
+    Ok(())
 }
-
