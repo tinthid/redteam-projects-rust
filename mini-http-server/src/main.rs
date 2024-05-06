7ef9b070919fbffca7b5d46b2f06ffb098866c3d
tinthid <dev@tinthid.com>
2024-05-06 09:57:53 +0700

    mini-http-server initial version

--- mini-http-server/src/main.rs
+++ mini-http-server/src/main.rs
@@ -1,12 +1,12 @@
 use axum::{
-    body::{Bytes, Body},
+    body::Body,
     http::{StatusCode, Request},
-    response::{Html, IntoResponse},
+    response::{self, IntoResponse, Response},
     routing::get,
     Router,
 };
 use maud::{html, Markup};
-use std::{fs, io};
+use std::fs;
 
 #[tokio::main]
 async fn main() {
@@ -19,31 +19,59 @@ async fn main() {
 
 }
 
-async fn handler(req: Request<Body>) -> Markup {
+async fn handler(req: Request<Body>) -> Response {
 
     let mut path = req.uri().path()[1..].to_string();
+    let link_path = path.clone();
+
+
+
     path.insert_str(0, "./");
 
     let metadata = match fs::metadata(path.clone()) {
         Ok(metadata) => metadata,
         Err(_) => {
-            return html! {
-                h1 { (format!("Error accessing {}", path)) }
-            };
+            return response::Response::builder()
+                .status(StatusCode::NOT_FOUND)
+                .body(Body::from("File not found"))
+                .unwrap();
         }
     };
 
+    if metadata.is_file() {
+        let file_data = std::fs::read(path.clone()).unwrap();
+        return response::Response::builder()
+                .status(StatusCode::OK)
+                .body(Body::from(file_data))
+                .unwrap();
+    }
 
-    let entries = fs::read_dir("./").unwrap()
-    .map(|res| res.map(|e| (e.file_name(), e.path().is_dir())))
+
+    let entries = fs::read_dir(path.clone()).unwrap()
+    .map(|res| res.map(|e| e.file_name().into_string().unwrap() ))
     .collect::<Result<Vec<_>, _>>().unwrap();
 
-    html! {
-        h1 { (path) }
+    let markup = html! {
+        h1 { (format!("/{}", link_path)) }
         ol {
             @for e in &entries { 
-                li { (e.0.clone().into_string().unwrap()) }
+                @if link_path.is_empty() {
+                    li { a href={(format!("/{}", e))} 
+                        { (e) } }
+                } @else {
+                    li { a href={(format!("/{}/{}", link_path, e))} 
+                        { (e) } }
+                }
             }
         }
+    };
+    return Html(markup).into_response();
+}
+
+struct Html(Markup);
+
+impl IntoResponse for Html {
+    fn into_response(self) -> Response {
+        response::Html(self.0.into_string()).into_response()
     }
 }
